075df857c73c3c133aa2b08bc778de8eb6959160
Zachary Boelter <zboelter@gmail.com>
2022-03-03 15:58:09 -0600

    pop and shift buttons with functionallity

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const emojiInput = document.getElementById("emoji-input")
 const emojiContainer = document.getElementById("emoji-container")
 const unshiftBtn = document.getElementById("unshift-btn")
 const pushBtn = document.getElementById("push-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
 
 
@@ -33,4 +35,16 @@ unshiftBtn.addEventListener("click", function(){
         emojiContainer.innerHTML = ""
         renderEmojis()
     }
+})
+
+shiftBtn.addEventListener("click", function(){
+    myEmojis.shift();
+    emojiContainer.innerHTML = ""
+    renderEmojis()
+})
+
+popBtn.addEventListener("click", function(){
+    myEmojis.pop();
+    emojiContainer.innerHTML = ""
+    renderEmojis()
 })
